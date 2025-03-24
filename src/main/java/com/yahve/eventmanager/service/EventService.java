59acd0e54d5b6708c2bc62ddd78b88c0c5258fdf
Ilya Skorokhodov <ilik124darc@gmail.com>
2025-03-24 08:40:28 +0200

    fixes

--- src/main/java/com/yahve/eventmanager/service/EventService.java
+++ src/main/java/com/yahve/eventmanager/service/EventService.java
@@ -20,12 +20,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
 
